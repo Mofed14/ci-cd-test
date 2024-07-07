fd3f5c582f6bd30012bb59dda1685dc20c1bf735
Mofed14 <mofed-salah-hana@gmail.com>
2024-07-07 23:14:52 +0300

    test branches rules

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/api/permissions', (req, res) => {
   console.log('test permissions');
 });
 
+app.get('/api/rules', (req, res) => {
+  console.log('test permissions');
+});
+
 app.listen(5000, () => {
   console.log('listening on port 5000');
 });
