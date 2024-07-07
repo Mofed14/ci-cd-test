311b671868a17a9a05c9723f43a47f6053a03feb
Mofed14 <mofed-salah-hana@gmail.com>
2024-07-08 00:06:44 +0300

    push

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.get('/api/permissions', (req, res) => {
+  console.log('test permissions');
+});
+
+app.get('/api/rules', (req, res) => {
+  console.log('test rules');
+});
+
 app.listen(5000, () => {
   console.log('listening on port 5000');
 });
