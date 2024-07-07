47e94941e2480bbb7be4d9e738f81e6bd2b63ba1
Mofed14 <mofed-salah-hana@gmail.com>
2024-07-07 23:05:52 +0300

    test permissions

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/api/permissions', (req, res) => {
+  console.log('test permissions');
+});
+
 app.listen(5000, () => {
   console.log('listening on port 5000');
 });
